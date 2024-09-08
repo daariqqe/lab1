e4fd9755775f4748a3dbbe95e47f57154a0f4add
Юра будет дуть <  @DAARISHITT>
2024-09-08 14:50:28 +0300

    Добавьте файлы проекта.

---
+++ lab1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task 1
+            string name = "Молдован Дар'я";
+            int age = 17;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Завдання 1");
+            Console.WriteLine("Мене звати {0}, мені років {1}", name, age);
+            Console.WriteLine();
+
+            //task 2 
+            int a = 3;
+            int b = 4;
+            double c;
+
+            c = (Math.Pow(a + b, 3) - Math.Pow(a, 3f)) / ((3 * a * Math.Pow(b, 2)) + Math.Pow(b, 3) + (3 * Math.Pow(a, 2) * b));
+            Console.WriteLine("Завдання 2");
+            Console.WriteLine($"{c}");
+            Console.WriteLine();
+
+            //task  3
+            Console.WriteLine("Завдання 3");
+            Console.WriteLine("Введіть трицифрове число та натисніть Enter ");
+            int last, mid, first, swap;
+            int q = Convert.ToInt32(Console.ReadLine());
+            last = q % 10;
+            mid = (q / 10) % 10;
+            first = q / 100;
+            swap = last * 100 + mid * 10 + first;
+            Console.WriteLine(swap);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ lab1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("lab1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("1a60e584-48bb-44b8-ac58-d965733369ef")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
